63f0ac89a6d8e2589ecda3fafe5b9eabc644c35b
Patrick Hagège <hagege.patrick@gmail.com>
2022-02-10 22:38:08 +0100

    css in progress

--- src/components/Forms/Searchbar.jsx
+++ src/components/Forms/Searchbar.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const SearchBar = ({ searchCallback }) => {
   return (
     <form id='searchbar' className='searchbar'>
-      <label htmlFor="search">Search your movie</label>
+      <label htmlFor="search"></label>
       <input
         type="text"
+        placeholder="Search a movie"
         name="search"
+        textalign="left"
         onChange={(e) => searchCallback(e.target.value)}
       />
     </form>
